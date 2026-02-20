9aa979b6a8d8cc617ae50b3227a0dc336b26b6f3
Claudio Coutinho <kraudym007@gmail.com>
2026-02-20 06:09:19 -0300

    Criando o método area na classe Triangle para obter os benefícios de reaproveitamento e delegação

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -6,4 +6,9 @@ public class Triangle {
 	public double b;
 	public double c;
 	
+	public double area() {
+		double P = (a + b + c) / 2.00;
+		return Math.sqrt(P* (P - a) * (P - b) * (P - c));
+	}
+	
 }
